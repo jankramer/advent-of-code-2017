b0343290bc52b00110c03e0f4bd8f19d5e6b1414
Jan Kramer <jan@jankramer.eu>
2017-12-02 09:40:13 +0100

    Simplify solution to day 2

--- Day2.hs
+++ Day2.hs
@@ -1,8 +1,10 @@
 module Day2 where
 
+import Data.List
+
 main = do
     input <- readFile "inputs/day2.txt"
-    let spreadsheet = map (map read . words) . lines $ input
+    let spreadsheet = map (reverse . sort . map read . words) . lines $ input
     print $ solve rowChecksum1 spreadsheet
     print $ solve rowChecksum2 spreadsheet
 
@@ -10,14 +12,10 @@ solve :: ([Int] -> Int) -> [[Int]] -> Int
 solve checksumFn spreadsheet = sum . map checksumFn $ spreadsheet
 
 rowChecksum1 :: [Int] -> Int
-rowChecksum1 values = (maximum values) - (minimum values)
+rowChecksum1 values = head values - last values
 
 rowChecksum2 :: [Int] -> Int
 rowChecksum2 (x:xs)
-    | length divisibles == 0    = rowChecksum2 xs
-    | otherwise                 = (max x y) `div` (min x y)
-    where divisibles = filter (isDivisiblePair x) xs
-          y          = head divisibles
-
-isDivisiblePair :: Int -> Int -> Bool
-isDivisiblePair a b = (max a b) `mod` (min a b) == 0
+    | null divisibles = rowChecksum2 xs
+    | otherwise       = x `div` head divisibles
+    where divisibles  = filter (\y -> x `mod` y == 0) xs
