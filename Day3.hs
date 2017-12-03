796ce12083f89cadee1e216c8ea7b51787393f42
Jan Kramer <jan@jankramer.eu>
2017-12-03 11:57:38 +0100

    Add initial solution to day 3 part 2

--- Day3.hs
+++ Day3.hs
@@ -1,6 +1,7 @@
 module Day3 where
 
 import Data.List
+import Data.Maybe
 
 data Move = Move { direction :: Direction
                  , steps :: Int
@@ -12,14 +13,36 @@ data Direction = U | D | R | L deriving (Show, Eq)
 data Point = Point { x :: Int
                    , y :: Int
                    } deriving (Show, Eq)
+origin = Point { x = 0, y = 0 }
 
+data ValuedPoint = ValuedPoint { point :: Point, value :: Int } deriving (Show)
+origin' = ValuedPoint { point = origin, value = 1 }
 
 main = do
     let input = 361527
-    print $ solve input
+    print $ part1 input
+    print $ part2 input
 
-solve :: Int -> Int
-solve n = manhattanDistance Point { x = 0, y = 0 } (last (take n generateSpiral)) - 1
+part1 :: Int -> Int
+part1 n = manhattanDistance origin (last (take n generateSpiral)) - 1
+
+part2 :: Int -> Int
+part2 n = value . fromJust $ find (\x -> value x > n) $ generateSequence generateSpiral [origin']
+
+generateSequence :: [Point] -> [ValuedPoint] -> [ValuedPoint]
+generateSequence (x:xs) valuedPoints = newPoint : generateSequence xs (newPoint : valuedPoints)
+    where newPoint = (calculateValuedPoint x valuedPoints)
+
+
+calculateValuedPoint :: Point -> [ValuedPoint] -> ValuedPoint
+calculateValuedPoint p prev = ValuedPoint { point = p
+                                          , value = sum . map value $ (filter (\q -> neighbours p (point q)) prev)
+                                          }
+
+neighbours :: Point -> Point -> Bool
+neighbours (Point x1 y1) (Point x2 y2) = (diffX == 0 || diffX == 1) && (diffY == 0 || diffY == 1)
+    where diffX = abs $ x2 - x1
+          diffY = abs $ y2 - y1
 
 generateSpiral :: [Point]
 generateSpiral = concat points
