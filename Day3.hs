8f2417cb772d364b48525d7a830b87e60eb8f0fa
Jan Kramer <jan@jankramer.eu>
2017-12-03 12:23:40 +0100

    Refactor solutions to day 3

--- Day3.hs
+++ Day3.hs
@@ -3,20 +3,16 @@ module Day3 where
 import Data.List
 import Data.Maybe
 
-data Move = Move { direction :: Direction
-                 , steps :: Int
-                 } deriving (Show, Eq)
+type SpiralGrid = [Point]
+data Point = Point { x :: Int, y :: Int } deriving (Show, Eq)
+origin = Point 0 0
 
+type SpiralGrid' = [Square]
+data Square = Square { point :: Point, value :: Int } deriving (Show)
+origin' = Square { point = origin, value = 1 }
+
+data Move = Move { direction :: Direction , steps :: Int } deriving (Show)
 data Direction = U | D | R | L deriving (Show, Eq)
-
-
-data Point = Point { x :: Int
-                   , y :: Int
-                   } deriving (Show, Eq)
-origin = Point { x = 0, y = 0 }
-
-data ValuedPoint = ValuedPoint { point :: Point, value :: Int } deriving (Show)
-origin' = ValuedPoint { point = origin, value = 1 }
 
 main = do
     let input = 361527
@@ -24,36 +20,19 @@ main = do
     print $ part2 input
 
 part1 :: Int -> Int
-part1 n = manhattanDistance origin (last (take n generateSpiral)) - 1
+part1 n = manhattanDistance origin (last (take n generateSpiralGrid))
 
-part2 :: Int -> Int
-part2 n = value . fromJust $ find (\x -> value x > n) $ generateSequence generateSpiral [origin']
+manhattanDistance :: Point -> Point -> Int
+manhattanDistance (Point x1 y1) (Point x2 y2) = (abs $ x2 - x1) + (abs $ y2 - y1)
 
-generateSequence :: [Point] -> [ValuedPoint] -> [ValuedPoint]
-generateSequence (x:xs) valuedPoints = newPoint : generateSequence xs (newPoint : valuedPoints)
-    where newPoint = (calculateValuedPoint x valuedPoints)
+generateSpiralGrid :: SpiralGrid
+generateSpiralGrid = concat $ scanl applyMove [origin] generateMoves
 
+applyMove :: [Point] -> Move -> [Point]
+applyMove points move = walk move (last points)
 
-calculateValuedPoint :: Point -> [ValuedPoint] -> ValuedPoint
-calculateValuedPoint p prev = ValuedPoint { point = p
-                                          , value = sum . map value $ (filter (\q -> neighbours p (point q)) prev)
-                                          }
-
-neighbours :: Point -> Point -> Bool
-neighbours (Point x1 y1) (Point x2 y2) = (diffX == 0 || diffX == 1) && (diffY == 0 || diffY == 1)
-    where diffX = abs $ x2 - x1
-          diffY = abs $ y2 - y1
-
-generateSpiral :: [Point]
-generateSpiral = concat points
-    where (_, points) = mapAccumL applyMove' Point{ x = 0, y = 0 } generateMoves
-
-applyMove' :: Point -> Move -> (Point, [Point])
-applyMove' previousPoint move = (last points, points)
-    where points = applyMove move previousPoint
-
-applyMove :: Move -> Point -> [Point]
-applyMove move point
+walk :: Move -> Point -> [Point]
+walk move point
     | d == U = map (\i -> Point { x = (x point)    , y = (y point) + i }) [1..n]
     | d == D = map (\i -> Point { x = (x point)    , y = (y point) - i }) [1..n]
     | d == R = map (\i -> Point { x = (x point) + i, y = y point       }) [1..n]
@@ -62,17 +41,34 @@ applyMove move point
           n = steps move
 
 generateMoves :: [Move]
-generateMoves = iterate generateMove Move { direction = R, steps = 1 }
+generateMoves = iterate getNextMove initialMove
+initialMove = Move R 1
 
-generateMove :: Move -> Move
-generateMove previousMove
-    | previousDirection == R = Move { direction = U, steps = previousSteps     }
-    | previousDirection == U = Move { direction = L, steps = previousSteps + 1 }
-    | previousDirection == L = Move { direction = D, steps = previousSteps     }
-    | previousDirection == D = Move { direction = R, steps = previousSteps + 1 }
+getNextMove :: Move -> Move
+getNextMove previousMove
+    | previousDirection == R = Move U previousSteps
+    | previousDirection == U = Move L (previousSteps + 1)
+    | previousDirection == L = Move D previousSteps
+    | previousDirection == D = Move R (previousSteps + 1)
     where
           previousDirection = direction previousMove
           previousSteps     = steps previousMove
 
-manhattanDistance :: Point -> Point -> Int
-manhattanDistance (Point x1 y1) (Point x2 y2) = (abs $ x2 - x1) + (abs $ y2 - y1)
+part2 :: Int -> Int
+part2 n = value . fromJust
+            $ find (\x -> value x > n)
+            $ generateSpiralGrid' (drop 1 $ generateSpiralGrid) [origin']
+
+generateSpiralGrid' :: [Point] -> [Square] -> [Square]
+generateSpiralGrid' (x:xs) prevSquares = newPoint : generateSpiralGrid' xs (newPoint : prevSquares)
+    where newPoint = (getNextSquare x prevSquares)
+
+getNextSquare :: Point -> [Square] -> Square
+getNextSquare p prev = Square { point = p
+                              , value = sum . map value $ (filter (\q -> neighbours p (point q)) prev)
+                              }
+
+neighbours :: Point -> Point -> Bool
+neighbours (Point x1 y1) (Point x2 y2) = (diffX == 0 || diffX == 1) && (diffY == 0 || diffY == 1)
+    where diffX = abs $ x2 - x1
+          diffY = abs $ y2 - y1
