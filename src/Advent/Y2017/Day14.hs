a60d91e2c1550e0206854564a2ff8d36506a1e79
Jan Kramer <jan@jankramer.eu>
2017-12-15 05:53:47 +0100

    Add solution to day 14

---
+++ src/Advent/Y2017/Day14.hs
@@ -0,0 +1,40 @@
+module Advent.Y2017.Day14 (day14a, day14b) where
+
+import Advent.Y2017.Day10   (knotHash)
+import Data.Graph           (components, graphFromEdges)
+import Data.Maybe           (isJust, fromJust)
+import Text.Printf          (printf)
+
+day14a, day14b :: String -> String
+day14a input = show $ length . filter (== '1') $ concat $ grid input
+day14b input = show $ length . components $ g
+    where (g, _, _) = graphFromEdges $ graphEntries $ grid input
+
+grid :: String -> [String]
+grid input = map (knotHashToBinary . knotHash) (map (\x -> input ++ ('-' : show x)) [0..127])
+
+knotHashToBinary :: [Int] -> String
+knotHashToBinary input = concat $ map (printf "%08b") $ input
+
+gridSize = 127
+
+graphEntries :: [String] -> [(Int, Int, [Int])]
+graphEntries grid = map fromJust $ filter isJust $ map (graphEntry grid) (map cellLoc [0..((gridSize+1)*(gridSize+1) - 1)])
+
+graphEntry :: [String] -> (Int,Int) -> Maybe (Int, Int, [Int])
+graphEntry grid (x,y)
+    | not $ isActive grid (x,y) = Nothing
+    | otherwise               = Just (cellId (x,y), cellId (x,y), map cellId activeNeighbours)
+    where activeNeighbours = filter (isActive grid) (neighbours (x,y))
+
+cellId :: (Int,Int) -> Int
+cellId (x,y) = x + y * (gridSize+1)
+
+cellLoc :: Int -> (Int,Int)
+cellLoc n = (n `mod` (gridSize+1), n `div` (gridSize+1))
+
+isActive :: [String] -> (Int, Int) -> Bool
+isActive grid (x,y) = grid !! y !! x == '1'
+
+neighbours :: (Int,Int) -> [(Int,Int)]
+neighbours (x,y) = filter (\(x,y) -> x >= 0 && x <= gridSize && y >= 0 && y <= gridSize) [(x, y-1), (x-1, y), (x+1, y), (x, y+1)]
